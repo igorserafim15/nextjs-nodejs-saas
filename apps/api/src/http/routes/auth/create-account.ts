41821a096adb16e91269a4aed2f0242e5e9c0158
Igor Serafim <igorserafim.contato@gmail.com>
2024-05-04 16:26:27 -0300

    .

--- apps/api/src/http/routes/auth/create-account.ts
+++ apps/api/src/http/routes/auth/create-account.ts
@@ -1,6 +1,9 @@
+import { hash } from 'bcryptjs'
 import { FastifyInstance } from 'fastify'
 import { ZodTypeProvider } from 'fastify-type-provider-zod'
 import z from 'zod'
+
+import { prisma } from '@/lib/prisma'
 
 export async function createAccount(app: FastifyInstance) {
   const bodySchema = z.object({
@@ -11,7 +14,33 @@ export async function createAccount(app: FastifyInstance) {
 
   app
     .withTypeProvider<ZodTypeProvider>()
-    .post('/users', { schema: { body: bodySchema } }, () => {
-      return 'User created'
-    })
+    .post(
+      '/users',
+      { schema: { body: bodySchema } },
+      async (request, response) => {
+        const body = request.body
+
+        const userWithSameEmail = await prisma.user.findUnique({
+          where: { email: body.email },
+        })
+
+        if (userWithSameEmail) {
+          return response
+            .status(409)
+            .send({ message: 'user with same email already exists. ' })
+        }
+
+        const passwordHash = await hash(body.password, 6)
+
+        await prisma.user.create({
+          data: {
+            name: body.name,
+            email: body.email,
+            passwordHash,
+          },
+        })
+
+        return response.status(201).send()
+      },
+    )
 }
